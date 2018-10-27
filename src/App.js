f148a002dd59212c93fcfcbc7297ed27191bab07
Sohardh <31403178+Sohardh@users.noreply.github.com>
2018-10-28 00:12:18 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './Container/Home/home';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Home />
+      </div>
+    );
+  }
+}
+
+export default App;
